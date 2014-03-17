2d55a00cf961026b2f9c1be49e060b612316d9cc
James Gennoy <james@gennoy.com>
2014-03-17 12:28:15 -0700

    Add Compass support and the ability to use regular CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
       },
       sass: {
         files: ['sass/*'],
-        tasks: ['sass', 'cssmin']
+        tasks: ['compass', 'cssmin']
       },
       css: {
         files: ['css/*'],
