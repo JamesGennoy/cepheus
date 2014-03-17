2c76fd52e6434a6334d258e2263a8acde0ae9ee2
James Gennoy <james@gennoy.com>
2014-03-16 17:04:07 -0700

    Added livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function(grunt) {
     },
     watch: {
       options: {
-        livereload: true,
+        livereload: true
       },
       html: {
         files: ['**/*.html', '!dist/**'],
